1e73ff8431329b0ed16906b55e734c13714aea59
Ashwini Sharma <83415972+ashwini286@users.noreply.github.com>
2025-02-06 11:37:16 +0530

    change icone of tab and also change color

--- App.js
+++ App.js
@@ -6,16 +6,50 @@ import Home from './container/screen/home';
 import Product from './container/screen/product';
 import Profile from './container/screen/profile';
 import Search from './container/screen/search';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Feather from 'react-native-vector-icons/Feather';
+
 
 // const Stack = createNativeStackNavigator(); 
 const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
   return (
     <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen name="Product" component={Product} />
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Product" component={Product} 
+      options={{
+        tabBarActiveTintColor: 'red',
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome name="product-hunt" color={color} size={size} />
+        ),
+      }}
+
+      />
+      <Tab.Screen name="Home" component={Home} 
+      options={{
+        tabBarActiveTintColor: 'purple',
+        tabBarIcon: ({ color, size }) => (
+          <AntDesign name="home" color={color} size={size} />
+        ),
+      }}
+      />
+      <Tab.Screen name="Profile" component={Profile} 
+        options={{
+          tabBarActiveTintColor: 'blue',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="face-man-profile" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen name="Search" component={Search}
+      options={{
+        tabBarActiveTintColor: 'green',
+        tabBarIcon: ({ color, size }) => (
+          <Feather name="search" color={color} size={size} />
+        ),
+      }}
+       />
     </Tab.Navigator>
   );
 };
--- container/screen/home.js
+++ container/screen/home.js
@@ -20,7 +20,7 @@ const home = ({ navigation }) => {
       />
       <Button
         title="Go to Profile"
-        onPress={() => navigation.navigate("Profile", { id: 2, name: "shreya" })}
+        onPress={() => navigation.navigate("Profile")}
       />
     </View>
   );
